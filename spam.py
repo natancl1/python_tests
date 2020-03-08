5813f69b75885a23d1e68513793d2b2be536e948
Natan <natan.camargo.louzada@hotmail.com>
2020-03-08 11:53:35 -0300

    Adding slots_test.py and spam.py files. And tests_advanced_class_topics.ipynb modified. All from learning python book.

---
+++ spam.py
@@ -0,0 +1,10 @@
+def printNumInstances():
+    print("Number of instances created: %s" % Spam.numInstances)
+
+class Spam:
+    numInstances = 0
+    def __init__(self):
+        Spam.numInstances = Spam.numInstances + 1
+    def printNumInstances(self):
+        print("Number of instances created: %s" % Spam.numInstances)
+
