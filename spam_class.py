590294413ca1b7256857dce62fba5c60cbb17a4f
Natan <natan.camargo.louzada@hotmail.com>
2020-03-14 16:26:02 -0300

    Add bothmethods.py, spam_class.py, spam_class2.py and spam_static.py files. tests_advanced_class_topics.ipyng file modified from learning python book

---
+++ spam_class.py
@@ -0,0 +1,16 @@
+class Spam:
+    numInstances = 0 # Use class method instead of static
+    def __init__(self):
+        Spam.numInstances += 1
+    def printNumInstances(cls):
+        print("Number of instances: %s %s" % (cls.numInstances, cls))
+    printNumInstances = classmethod(printNumInstances)
+
+class Sub(Spam):
+    def printNumInstances(cls): # Override a class method
+        print("Extra stuff...", cls) # But call back to original
+        Spam.printNumInstances()
+    printNumInstances = classmethod(printNumInstances)
+
+class Other(Spam): pass # Inherit class method.verbatim
+
